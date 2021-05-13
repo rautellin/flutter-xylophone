234edb18eb5ac08de6e1285b504305a4b09e16cb
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2021-05-13 12:10:03 +0200

    finished the app

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Music app that plays Xylophone sounds.
 
 ## What I learned
 
-- How to incorporate open source libraries of code into your project using Flutter Packages.
+- How to incorporate open source libraries of code into the project using Flutter Packages.
 - How to play sound on both iOS and Android.
 - How to generate repeated user interface Widgets.
 - How to use Dart functions that can take input arguments as well as return an output.
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int number) {
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
+  Widget buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: ElevatedButton(
+        child: null,
+        style: ElevatedButton.styleFrom(primary: color),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.lightGreenAccent, soundNumber: 4),
+              buildKey(color: Colors.green, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
